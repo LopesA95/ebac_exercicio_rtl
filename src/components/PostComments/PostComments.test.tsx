77f7d484ce5dcd97373c75e0582291834abd4215
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-04-12 15:35:54 -0400

    ✨ teste completo

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,27 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar comentários corretamente', () => {
+        render(<PostComment />);
+        const input = screen.getByTestId('comment-input');
+        const button = screen.getByTestId('add-comment-button');
+
+        // Adiciona o primeiro comentário
+        fireEvent.change(input, { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(button);
+
+        // Adiciona o segundo comentário
+        fireEvent.change(input, { target: { value: 'Segundo comentário' } });
+        fireEvent.click(button);
+
+        // Verifica se os comentários foram adicionados
+        expect(screen.getByTestId('comment-0')).toHaveTextContent('Primeiro comentário');
+        expect(screen.getByTestId('comment-1')).toHaveTextContent('Segundo comentário');
     });
 });
